88685e21ac86f9f4078e76a0457ef01a1f4e7f3d
aleksnew2 <aleks.koyf@gmail.com>
2024-12-29 22:05:20 +0300

    add: module system

---
+++ system/current.go
@@ -0,0 +1,19 @@
+package system
+
+import (
+	"deltarune_engine/exception"
+	"os"
+)
+
+// Current returns the hostname of the current operating system.
+// It retrieves the hostname using the os.Hostname() function.
+// If there is an error retrieving the hostname, it returns an empty string and an error.
+// Otherwise, it returns the hostname prefixed with "OS: ".
+func Current() (string, error) {
+	name, err := os.Hostname()
+	if err != nil {
+		return "", exception.CreateException(-1, "")
+	}
+
+	return "OS: " + name, nil
+}
