0d94d1471a7e4a16b57ba971a24ede138880dd8d
aleksnew2 <aleks.koyf@gmail.com>
2024-12-29 21:55:29 +0300

    global: make docs

--- io/console/console.go
+++ io/console/console.go
@@ -9,14 +9,21 @@ type Console struct {
 	Settings Settings
 }
 
+// Log prints the provided arguments to the console, formatted according to the Console's settings.
+// It accepts a variable number of string arguments.
 func (c Console) Log(args ...string) {
 	fmt.Println(format(c.Settings, message.Info, args...))
 }
 
+// Error prints a formatted error message to the console.
+// The error message is formatted based on the provided error code and additional arguments.
 func (c Console) Error(error_code int32, args ...string) {
 	fmt.Println(formatException(c.Settings, error_code, args...))
 }
 
+// Warning logs a warning message to the console.
+// It takes an error code and a variadic number of string arguments
+// which are used to format the warning message.
 func (c Console) Warning(error_code int32, args ...string) {
 	fmt.Println(format(c.Settings, message.Warning, args...))
 }
--- io/console/message/format_message.go
+++ io/console/message/format_message.go
@@ -13,6 +13,14 @@ func convertEnumIntoString(message_type Type) string {
 	return ""
 }
 
+// Format converts a message type enum into its corresponding string representation.
+// It takes a parameter of type `Type` and returns the string representation of that type.
+//
+// Parameters:
+//   - message_type: The message type enum to be converted.
+//
+// Returns:
+//   - A string representing the message type.
 func Format(message_type Type) string {
 	return convertEnumIntoString(message_type)
 }
