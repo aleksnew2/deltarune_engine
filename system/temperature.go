15d94b085e5ad1ae2318d378745143c93bf7108b
aleksnew2 <aleks.koyf@gmail.com>
2024-12-30 11:35:01 +0300

    create: temperature.go

---
+++ system/temperature.go
@@ -0,0 +1,25 @@
+package system
+
+import (
+	"deltarune_engine/exception"
+
+	"github.com/shirou/gopsutil/host"
+)
+
+func GetCurrentTemperature() (string, float64, error) {
+	var temperature float64
+	var sensorKey string
+
+	temps, err := host.SensorsTemperatures()
+
+	if err != nil {
+		return "", 0.0, exception.CreateException(-1, err.Error())
+	}
+
+	for _, t := range temps {
+		temperature = t.Temperature
+		sensorKey = t.SensorKey
+	}
+
+	return sensorKey, temperature, nil
+}
