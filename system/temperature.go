507234438cca0439dc82cbc24c92de9a597fe3a9
aleksnew2 <aleks.koyf@gmail.com>
2024-12-30 11:39:52 +0300

    docs: add function documentation for GetCurrentTemperature

--- system/temperature.go
+++ system/temperature.go
@@ -6,6 +6,10 @@ import (
 	"github.com/shirou/gopsutil/host"
 )
 
+// GetCurrentTemperature retrieves the current system temperature reading from hardware sensors.
+// It returns the temperature value as a float64 in Celsius and an error if the reading fails.
+// If multiple temperature sensors are present, returns the last sensor's reading.
+// Returns 0.0 and an error if unable to access temperature sensors.
 func GetCurrentTemperature() (float64, error) {
 	var temperature float64
 	temps, err := host.SensorsTemperatures()
