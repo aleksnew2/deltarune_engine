37cc61cd4bb538e9143b1121c2e8175251727492
aleksnew2 <aleks.koyf@gmail.com>
2024-12-31 19:26:17 +0300

    join.go: add docs

--- utils/join.go
+++ utils/join.go
@@ -5,6 +5,11 @@ import (
 	"strings"
 )
 
+// Join concatenates the elements of the provided slice into a single string
+// with the specified separator between each element. The elements of the slice
+// are converted to their string representations using fmt.Sprint.
+//
+// T is a type parameter that can be any type.
 func Join[T any](slice []T, sep string) string {
 	strs := make([]string, len(slice))
 	for i, v := range slice {
